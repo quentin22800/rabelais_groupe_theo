adb7d10364e318ca5a1c8bd96fc3ce4b1d0960b3
Quentin <Quentin@192.168.1.69>
2015-11-23 14:33:12 +0100

    Ajout du module "Gestion rentabilité"

---
+++ src/gestioninscription/ModeleJTableParticipant.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestioninscription;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Quentin
+ */
+public class ModeleJTableParticipant extends AbstractTableModel
+{
+    private String[] columnNames = {"Matricule","Nom","Taux horaire"};
+	private Object[][] data=new Object[10][8];
+	public int getColumnCount()
+	{
+		return columnNames.length;
+	}
+	public int getRowCount()
+	{
+		return data.length;
+	}
+	public String getColumnName(int col)
+	{
+		return columnNames[col];
+	}
+	public Object getValueAt(int row, int col)
+	{
+		return data[row][col];
+	}
+	
+	public boolean isCellEditable(int row, int col)
+	{
+		return (col < 2);
+	}
+	public void setValueAt(Object value, int row, int col)
+	{
+		data[row][col] = value;
+		fireTableCellUpdated(row,col);
+		//fireTableDataChanged();
+	}
+}
